2ce3675946ca7bcf1a0f847213ea631529bde518
ShahramShakiba <shahramshakiba88@gmail.com>
2024-07-02 19:14:54 +0330

    Initializing Raycaster and Mouse Events Project

---
+++ src/script.js
@@ -0,0 +1,72 @@
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import GUI from 'lil-gui';
+import * as THREE from 'three';
+
+const canvas = document.querySelector('canvas.webgl');
+const scene = new THREE.Scene();
+const gui = new GUI({ title: 'Physics' });
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+//================== Objects ========================
+const object1 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+object1.position.x = -2;
+
+const object2 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+
+const object3 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+);
+object3.position.x = 2;
+
+scene.add(object1, object2, object3);
+
+//================= Camera ==========================
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+camera.position.z = 3;
+scene.add(camera);
+
+//============== Orbit Controls =====================
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+//================= Renderer ========================
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(width, height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+//=============== Resize Listener ====================
+window.addEventListener('resize', () => {
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(width, height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+//=================== Animate ========================
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
