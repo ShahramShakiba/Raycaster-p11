124af5835ab1075b85a5ad5bc731ccabe2854cfe
ShahramShakiba <shahramshakiba88@gmail.com>
2024-07-03 12:55:09 +0330

    Cast a Ray and Update the Distance

--- src/script.js
+++ src/script.js
@@ -12,26 +12,48 @@ let height = window.innerHeight;
 //================== Objects ========================
 const object1 = new THREE.Mesh(
   new THREE.SphereGeometry(0.5, 16, 16),
-  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.MeshBasicMaterial({ color: '#b9ca3f' })
 );
 object1.position.x = -2;
+/* To get the right distance of ray casting: Threejs updates the objs' coordinates right before rendering them, since we do the ray casting immediately, none of the objs' coordinates have been rendered - They are all 2.5, while it should be obj1=0.5, obj2=2.5, obj3=4.5 */
+object1.updateMatrixWorld();
 
 const object2 = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 16, 16),
-  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.CylinderGeometry(0.5, 0.5, 1.5),
+  new THREE.MeshBasicMaterial({ color: '#b9ca3f' })
 );
+object2.updateMatrixWorld();
 
 const object3 = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 16, 16),
-  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.ConeGeometry(0.7, 1),
+  new THREE.MeshBasicMaterial({ color: '#b9ca3f' })
 );
 object3.position.x = 2;
+object3.updateMatrixWorld();
 
 scene.add(object1, object2, object3);
 
+//================ Raycaster ========================
+const raycaster = new THREE.Raycaster();
+
+const rayOrigin = new THREE.Vector3(-3, 0, 0);
+// Direction must have a length of 1, otherwise it should be normalized
+const rayDirection = new THREE.Vector3(10, 0, 0);
+// console.log(rayDirection.length());
+rayDirection.normalize();
+// console.log(rayDirection.length());
+
+raycaster.set(rayOrigin, rayDirection);
+
+const intersect = raycaster.intersectObject(object1);
+console.log(intersect);
+
+const intersects = raycaster.intersectObjects([object1, object2, object3]);
+console.log(intersects);
+
 //================= Camera ==========================
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-camera.position.z = 3;
+camera.position.z = 4;
 scene.add(camera);
 
 //============== Orbit Controls =====================
